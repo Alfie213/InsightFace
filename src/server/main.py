083f389a2889feca058af5a6b89c6e97da11bf04
Alfie213 <arturbogoratov@gmail.com>
2025-08-27 16:48:40 +0400

    Update main.py

--- src/server/main.py
+++ src/server/main.py
@@ -8,8 +8,8 @@ PROJECT_ROOT = current_file.parent.parent.parent # Move up 3 levels from main.py
 images_dir = PROJECT_ROOT / 'images'
 image_path = images_dir / 'image.jpg'
 
-from src.server.utils.utils_inference import get_lmks_by_img, get_model_by_name
-from src.server.utils.utils_landmarks import show_landmarks
+from utils.utils_inference import get_lmks_by_img, get_model_by_name
+from utils.utils_landmarks import show_landmarks
 
 # --- Main Logic ---
 try:
